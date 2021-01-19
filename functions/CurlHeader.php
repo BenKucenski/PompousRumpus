de662d4d6c34d3218b92dd5811c39a066657c233
Ben Kucenski <bkucenski@gmail.com>
2021-01-19 10:45:35 -0700

    don't use continue is a case statement with PHP 7.4

--- functions/CurlHeader.php
+++ functions/CurlHeader.php
@@ -66,7 +66,7 @@ class CurlHeader
                     $this->Location = $v;
                     break;
                 default:
-                    continue;
+                    break;
             }
         }
     }
